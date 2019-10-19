f04e04569e0ffcee1e9408a0dff066616f500a21
Mahadi <mahadirony.rony@gmail.com>
2019-10-20 01:51:37 +0600

    User created and login completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, user_logout, user_registration
 
 urlpatterns = [
-    url(r'', index, name='login')
+    url(r'', index, name='login'),
+    url(r'', user_logout, name='logout'),
+    url(r'registration', user_registration, name='registration')
 ]
